6e29f150191f23eab7ae89590dc227030418d91d
Kr <magic.rooter@gmail.com>
2026-04-23 18:38:27 +0300

    feat: Remove unnecessary ESLint configurations
    Removes deprecated FlatCompat import and default next/core-web-vitals extension from ESLint configuration. These are no longer needed with the current ESLint setup.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 export default [
   {
     ignores: [".next/*", "node_modules/*", "dist/*"],
   },
-  ...compat.extends("next/core-web-vitals"),
   {
     rules: {
       "react-hooks/exhaustive-deps": "off",
